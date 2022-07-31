37b387865f79fb156e3cec0593a8436a3e85ad67
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-07-31 22:21:26 +0200

    Create 31 square

--- main.js
+++ main.js
@@ -1,17 +1,22 @@
 const myApp = document.querySelector('.myApp');
-let btn = document.createElement('button');
 let numberDays = document.createElement('span');
-
-let counter;
+myApp.style.width = '600px';
+myApp.style.height = '600px';
+let daysNb=1;
 let itemDays = 31;
 function generateSquareMonth() {
-    for( let i = 0; i < 31; i++) {
-        for (counter=1; counter < itemDays; counter++) {
+    for( let i = 0; i < itemDays; i++) {
+        let btn = document.createElement('button');
+        for (let counter=1; counter < itemDays; counter++) {
             myApp.appendChild(btn);
             btn.classList.add('backgroundApp');
             btn.style.backgroundColor = 'red';
-            btn.innerHTML = counter;
+            btn.style.width = '90px';
+            btn.style.height = '90px';
+            btn.style.fontSize = '50px';
         }
+        btn.innerHTML = daysNb++;
+        // console.log(i)
     }
 }
 generateSquareMonth();
