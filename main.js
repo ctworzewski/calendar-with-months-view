c52da43124e18bc7f181a86a466cfdd4ab225bc2
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-01 12:13:33 +0200

    Create form calendar with JS _3

--- main.js
+++ main.js
@@ -1,50 +1,52 @@
-
-let myApp = document.querySelector('.myApp');
+let myApp = document.querySelector(".myApp");
 let iloscDniwMiesiacu = 31;
 
-const nameDays = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
-const boxNameDays = document.createElement('div');
-// boxNameDays.classList.add('boxNameDays');
-// const weekBox = document.createElement('div');
-boxNameDays.classList.add('boxNameDays');
+const nameDays = [
+  "Poniedziałek",
+  "Wtorek",
+  "Środa",
+  "Czwartek",
+  "Piątek",
+  "Sobota",
+  "Niedziela",
+];
+const boxNameDays = document.createElement("div");
+boxNameDays.classList.add("boxNameDays");
 
-let divMain = document.createElement('div');
-divMain.style.backgroundColor = 'green';
-divMain.style.width = '300px';
-divMain.style.height = '300px';
-let divSeven = document.createElement('div');
-let testDiv = document.createElement('div');
+let divMain = document.createElement("div");
+divMain.style.backgroundColor = "green";
+divMain.style.width = "300px";
+divMain.style.height = "300px";
+let divSeven = document.createElement("div");
+let testDiv = document.createElement("div");
 
 let iloscDni = 31;
 let oneDay = 1;
 let weekBox;
 
 function showNameDays() {
-    for (let i = 0; i <= nameDays.length; i++) {
-        const el = document.createElement('div');
-        el.classList.add('item-name');
-            for (let j = 0; j < i; j++) {
-                myApp.appendChild(boxNameDays);
-                boxNameDays.appendChild(el);
-                el.innerHTML = nameDays[j];
-            }
-     }
+  for (let i = 0; i <= nameDays.length; i++) {
+    const el = document.createElement("div");
+    el.classList.add("item-name");
+    for (let j = 0; j < i; j++) {
+      myApp.appendChild(boxNameDays);
+      boxNameDays.appendChild(el);
+      el.innerHTML = nameDays[j];
+    }
+  }
 }
 
 showNameDays();
-// let counter = 1;
-for (let i = 1; i <=1; i++) {
-    let divMain = document.createElement('div');
-    divMain.classList.add('divMain');
-    myApp.style.backgroundColor = 'yellow';
-    myApp.appendChild(divMain);
+for (let i = 1; i <= 1; i++) {
+  let divMain = document.createElement("div");
+  divMain.classList.add("divMain");
+  myApp.style.backgroundColor = "yellow";
+  myApp.appendChild(divMain);
 
-    for (let i =1; i <= iloscDni; i++) {
-        let btn = document.createElement('button');
-        btn.classList.add('btn');        
-        divMain.appendChild(btn);
-        btn.innerHTML = oneDay++;
-    }
+  for (let i = 1; i <= iloscDni; i++) {
+    let btn = document.createElement("button");
+    btn.classList.add("btn");
+    divMain.appendChild(btn);
+    btn.innerHTML = oneDay++;
+  }
 }
-
-
