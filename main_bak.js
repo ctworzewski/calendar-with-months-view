4e55254e0cd9c02a427532e7502617e0f17767c1
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-01 07:55:36 +0200

    Edit

--- main_bak.js
+++ main_bak.js
@@ -3,43 +3,41 @@ const nameDays = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'S
 const boxNameDays = document.createElement('div');
 
 boxNameDays.classList.add('boxNameDays');
-myApp.classList.add('myApp');
+boxNameDays.style.width = '600px';
+boxNameDays.style.fontSize = '25px';
+myApp.style.width = '600px';
+myApp.style.height = '600px';
 let daysNb=1;
 let itemDays = 31;
 function renderCalendar(){
     showNameDays();
+    generateSquareMonth();
 }
 function showNameDays() {
     for (let i = 0; i <= nameDays.length; i++) {
-        const el = document.createElement('div');
-        el.classList.add('item-name');
-
-         for (let j = 0; j < i; j++) {
-            // if (itemDays === nameDays.length){
-            //  for ( let i = 0; i < itemDays; i++) {
-            //     let btn = document.createElement('button');
-            //     }
-            // }
-                myApp.appendChild(boxNameDays);
-                boxNameDays.appendChild(el);
-                el.innerHTML = nameDays[j];
-            }
+        const el = document.createElement('span');
+        for (let j = 0; j < i; j++) {
+            myApp.appendChild(boxNameDays);
+            boxNameDays.appendChild(el);
+            boxNameDays.style.marginRight = '40px';
+            el.innerHTML = nameDays[j];
         }
-        generateSquareMonth();
+    }
 }
 
 function generateSquareMonth() {
     for( let i = 0; i < itemDays; i++) {
         let btn = document.createElement('button');
-        btn.classList.add('backgroundApp');
         for (let counter=1; counter < itemDays; counter++) {
             myApp.appendChild(btn);
+            btn.classList.add('backgroundApp');
+            btn.style.backgroundColor = 'red';
+            btn.style.width = '90px';
+            btn.style.height = '90px';
+            btn.style.fontSize = '50px';
         }
         btn.innerHTML = daysNb++;
     } 
 }
 
 renderCalendar();
-
-
-
