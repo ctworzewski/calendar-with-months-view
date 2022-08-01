9ec7facf26d21c34bd0585286ada64a36baa6aa0
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-01 05:54:21 +0200

    Render Monday static in DOM

--- main.js
+++ main.js
@@ -4,33 +4,39 @@ const nameDays = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'S
 
 
 // za zadanie muszę wyświetlić  nazwy dni tygodnia w DOM
+//Podziału na tygodnie. Najlepiej gdyby dni renderowaly się po 7 dni na tydzień. 1 tydzień = 1 rząd
 const boxNameDays = document.createElement('div');
-for (let i = 0; i < nameDays; i++) {
-    boxNameDays[i].innerHTML = nameDays[i];
-    
-}
+const el = document.createElement('span');
+el.innerHTML = nameDays[0];
 myApp.appendChild(boxNameDays);
+boxNameDays.appendChild(el);
+
+
+
+
+
 myApp.style.width = '600px';
 myApp.style.height = '600px';
 let daysNb=1;
 let itemDays = 31;
 function generateSquareMonth() {
+    //if ((itemDays >= 1) && (itemDays<=7)) {
     for( let i = 0; i < itemDays; i++) {
-        let btn = document.createElement('button');
-        for (let counter=1; counter < itemDays; counter++) {
-            myApp.appendChild(btn);
-            btn.classList.add('backgroundApp');
-            btn.style.backgroundColor = 'red';
-            btn.style.width = '90px';
-            btn.style.height = '90px';
-            btn.style.fontSize = '50px';
+
+
+            let btn = document.createElement('button');
+            for (let counter=1; counter < itemDays; counter++) {
+                myApp.appendChild(btn);
+                btn.classList.add('backgroundApp');
+                btn.style.backgroundColor = 'red';
+                btn.style.width = '90px';
+                btn.style.height = '90px';
+                btn.style.fontSize = '50px';
+            }
+            
+            btn.innerHTML = daysNb++;
         }
-        btn.innerHTML = daysNb++;
-        // console.log(i)
-    btn.addEventListener('click', function() {
-        console.log(btn.value);
-    });
-    }
+    //}
     
 }
 
