a7c4f219fb940545567eff3d3f71444ddcbf20bb
Cezary Tworzewski <cezary.tworzewski@gmail.com>
2022-08-01 07:38:46 +0200

    Small css

--- main.js
+++ main.js
@@ -1,61 +1,46 @@
 const myApp = document.querySelector('.myApp');
-// let numberDays = document.createElement('span');
 const nameDays = ['Poniedziałek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
 const boxNameDays = document.createElement('div');
 
 boxNameDays.classList.add('boxNameDays');
-boxNameDays.style.width = '600px';
-boxNameDays.style.fontSize = '25px';
-// boxNameDays.style.display = 'inline-block';
-// boxNameDays.style.marginRight = '40px';
-
-
-// za zadanie muszę wyświetlić  nazwy dni tygodnia w DOM
-//Podziału na tygodnie. Najlepiej gdyby dni renderowaly się po 7 dni na tydzień. 1 tydzień = 1 rząd
-// const el = document.createElement('span');
-// myApp.appendChild(boxNameDays);
-// boxNameDays.appendChild(el);
-
-myApp.style.width = '600px';
-myApp.style.height = '600px';
+myApp.classList.add('myApp');
 let daysNb=1;
 let itemDays = 31;
 function renderCalendar(){
     showNameDays();
-    generateSquareMonth();
 }
 function showNameDays() {
     for (let i = 0; i <= nameDays.length; i++) {
-        const el = document.createElement('span');
+        const el = document.createElement('div');
+        el.classList.add('item-name');
+
         for (let j = 0; j < i; j++) {
+             for( let i = 0; i < itemDays; i++) {
+                let dlWeek = 7;
+                if (itemDays === dlWeek){
+                    let btn = document.createElement('button');
+                }
+             }
             myApp.appendChild(boxNameDays);
             boxNameDays.appendChild(el);
-            boxNameDays.style.marginRight = '40px';
             el.innerHTML = nameDays[j];
         }
     }
+    generateSquareMonth();
 }
-
-
-
 
 function generateSquareMonth() {
     for( let i = 0; i < itemDays; i++) {
         let btn = document.createElement('button');
+        btn.classList.add('backgroundApp');
         for (let counter=1; counter < itemDays; counter++) {
             myApp.appendChild(btn);
-            btn.classList.add('backgroundApp');
-            btn.style.backgroundColor = 'red';
-            btn.style.width = '90px';
-            btn.style.height = '90px';
-            btn.style.fontSize = '50px';
         }
         btn.innerHTML = daysNb++;
     } 
 }
 
 renderCalendar();
-
 
 
 
